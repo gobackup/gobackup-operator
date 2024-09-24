1629f523f39a2f6ac68b2cf560c6566e8f41be8b
Payam Qorbanpour <payamqorbanpour@gmail.com>
2024-09-24 16:10:16 +0330

    Handle not found case specifically

--- pkg/k8sutil/crd.go
+++ pkg/k8sutil/crd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -23,6 +24,10 @@ func GetCRD(ctx context.Context, dynamicClient dynamic.Interface,
 	// Fetch the instance
 	crdObj, err := dynamicClient.Resource(gvr).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
 	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil, fmt.Errorf("CRD %s in namespace %s not found: %w", name, namespace, err)
+		}
+
 		return nil, fmt.Errorf("failed to fetch CRD %s in namespace %s: %w", name, namespace, err)
 	}
 
